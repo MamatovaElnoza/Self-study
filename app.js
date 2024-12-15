58fba914582dc7a9cb68941089090acaf4a3402d
MamatovaElnoza <mamatovaelnoza@gmail.com>
2024-12-15 22:38:45 +0500

    Any Example

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ console.log("Hello World!");
 //Arrays
 const numbers = [1, 2, 3];
 const anotherNumbers = [1, 2, 3];
+//Any
+const person = { firstname: "Elnoza" };
--- app.ts
+++ app.ts
@@ -3,3 +3,6 @@ console.log("Hello World!");
 //Arrays
 const numbers: number[] = [1, 2, 3];
 const anotherNumbers: Array<number> = [1, 2, 3];
+
+//Any
+const person: any = { firstname: "Elnoza" };
